ea7f7cbc1a0251a62de6380d47534bdd96328de3
neeraj5696 <neerajkumar5696@gmail.com>
2023-10-13 01:34:12 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,20 @@ function App() {
   return (
     <>
       <header>
-      <div className='flex'>
-      <div>
-        <img src='https://cimg.co/p/crypto-news-logo-full.svg' alt='logo'/>
+      <div className='flex p-25'>
+      <div className=''>
+        
+        <a href='www.google.com'> <img src='https://cimg.co/p/crypto-news-logo-full.svg' alt='logo'/></a>
         </div>
        
-        <div className='px-6 bold'>
+        <div className='pl-6 bold'>
           All
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="7" viewBox="0 0 12 7">
+              <g fill-rule="evenodd">
+                  <path d="M0 3.999L12 3.999 12 2.999 0 2.999zM0 7L12 7 12 6 0 6zM0 1L12 1 12 0 0 0z"></path>
+              </g>
+          </svg>
       </div>
       </header>
     </>
